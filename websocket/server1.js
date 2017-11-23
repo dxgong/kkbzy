ec0f7947fa04760e7f4080fbacb67f51ae65979e
CatDamon <dxgong1994313@163.com>
2017-11-23 15:28:16 +0800

    websocket聊天室

--- websocket/server1.js
+++ websocket/server1.js
@@ -15,6 +15,9 @@ const connection = mysql.createConnection({
 connection.connect();
 
 let server = http.createServer((req, res)=>{
+
+
+
   fs.readFile(`HTML${req.url}`, (err, data)=>{
     if(err){
       res.writeHeader(404);
@@ -28,9 +31,10 @@ let server = http.createServer((req, res)=>{
 }).listen(8081);
 
 const websocket = sk.listen(server);
-
+let aSock = [];
 websocket.on('connection',(socket)=>{
-
+  aSock.push(socket);
+  let cut_username = '';
   //注册
   socket.on('res',(username, password)=>{
     let selectParams = [];
@@ -77,5 +81,52 @@ websocket.on('connection',(socket)=>{
 
   //登录
 
+  socket.on('login',(username,password)=>{
+    let selectParams = [];
+    selectParams[0] = username;
+    selectParams[1] = password;
+
+    let selectStr = 'select username from ry_user where username = ? and password = ?';
+    connection.query(selectStr,selectParams, (err,data)=>{
+      if(err){
+        console.log(err);
+        socket.emit('login_ret',1,"查询错误");
+      }else{
+        if(data.length == 0){
+          console.log("用户名或者密码错误");
+          socket.emit('login_ret',1,"用户名或者密码错误");
+        }else{
+          let updateStr = 'update ry_user set available = 1 where username = ? and password = ?';
+          connection.query(updateStr,selectParams,(err,data)=>{
+            if(err){
+              console.log(err);
+              socket.emit('login_ret',1,"用户名或者密码不正确");
+            }else{
+              cut_username = username;
+              socket.emit('login_ret',0,"登录成功");
+            }
+          })
+        }
+      }
+    })
+  })
+
+  //发信息
+  socket.on('message',(msg)=>{
+    if(!msg){
+      socket.emit('msg_ret',1,'消息不能为空!');
+    }else{
+      aSock.forEach((item)=>{
+        if(item===socket)return;
+        console.log("当前发送信息的人是：" + cut_username);
+        console.log("当前发送的信息是：" + msg);
+
+
+        item.emit('message',cut_username,msg);
+      })
+      socket.emit('msg_ret',0,'发送成功!');
+    }
+
+  })
 
 })
